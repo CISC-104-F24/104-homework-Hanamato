6b1643fb804040a5cc0938523d7d6416a27b8d69
Hanamato <hanamato@udel.edu>
2024-09-06 14:29:08 -0400

    completes homework 0

--- Homework Project/Assets/Scenes/scripts/helloworld.cs
+++ Homework Project/Assets/Scenes/scripts/helloworld.cs
@@ -7,12 +7,20 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+     Debug.Log("Hello World!");   
+     Debug.LogWarning("Hello, I am a warning");
+     Debug.LogError("Error!");
+
+     //Author: Hannah A
+     /*
+      How complicated is the coding going to get? Adding characters, collisions, etc
+      A simply game where you can explore environments and solve puzzles could be fun to try to make
+      I want to find out more about how you would code character interactions, whether its dialogue tress or how npcs know how to respond to actions the player makes in the overworld
+     */  
     }
 
     // Update is called once per frame
     void Update()
     {
-     Debug.Log("Hello World!");   
     }
 }
