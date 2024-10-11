d008fcc9e4cf055a5422d508ab60e9ab31420e40
Hanamato <hanamato@udel.edu>
2024-10-11 17:53:34 -0400

    .
    iiiiiii. hateeeeeee unityyyyyyyyy

--- Homework Project/Assets/Wooper.cs
+++ Homework Project/Assets/Wooper.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class Wooper : MonoBehaviour
 { 
-public float baseSpeed = 1.0f;
-public float movementDirection;
+public float baseSpeed = 0.5f;
+public float jumpPower = 1.5f;
+public 
+
+bool isRightPressed = input.GetKey(KeyCode.W);
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Wooper is ready to explore!");
+        transform.position = new Vector3(1,1,1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + movementDirection * baseSpeed * Time.deltaTime;
-
+        
+        if (isRightPressed)
+        {transform.position = transform.position + new Vector3(1,0,0) * Time.deltaTime;}
     }
 }
