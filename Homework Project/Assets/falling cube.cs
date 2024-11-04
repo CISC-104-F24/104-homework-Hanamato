ce39db0f03c0e46e01b52a834845707762c4af9a
Hanamato <hanamato@udel.edu>
2024-11-04 16:39:47 -0500

    falling cube

--- Homework Project/Assets/falling cube.cs
+++ Homework Project/Assets/falling cube.cs
@@ -7,12 +7,19 @@ public class fallingcube : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(-20f,15f,0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+    
     }
+
+    void OnCollisionEnter (Collision collision)
+    {
+        transform.position = new Vector3(-20f,15f,0f);
+        //Debug.Log("Hiiiii");
+    }
+
 }
